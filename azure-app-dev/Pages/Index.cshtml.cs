8d9b2003155327e8e73f4387ce28c82c97c7c0f9
shrinayKshirsagar@outlook.com <shrinayKshirsagar@outlook.com>
2025-04-26 13:42:52 +0530

    added configuration

--- azure-app-dev/Pages/Index.cshtml.cs
+++ azure-app-dev/Pages/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace azure_app_dev.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly IConfiguration _configuration;
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
-            _logger = logger;
+            this._logger = logger;
+            this._configuration = configuration;
+
         }
 
         public void OnGet()
         {
-
+            ViewData["Greeting"] = _configuration["Greeting"];
         }
     }
 }
